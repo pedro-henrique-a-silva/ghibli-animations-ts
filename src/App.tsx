7d1424baaedcbeed0de1c53871098cae85249152
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2023-09-06 16:06:57 -0300

    Requisito 01 concluido, criando o context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import { Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import FilmsContext from './context/FilmsContext';
 
 import Home from './pages/Home';
 import Favorites from './pages/Favorites';
 
 function App() {
+  const [films, setFilms] = useState([]);
+
+  useEffect(() => {
+    const getFilms = async () => {
+      const response = await fetch('https://api-trybe-frontend.vercel.app/api/ghibli-animations');
+      const results = await response.json();
+      setFilms(results);
+    };
+
+    getFilms();
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" Component={ Home } />
-      <Route path="/favorites" Component={ Favorites } />
-    </Routes>
+    <FilmsContext.Provider value={ { films } }>
+      <Routes>
+        <Route path="/" Component={ Home } />
+        <Route path="/favorites" Component={ Favorites } />
+      </Routes>
+    </FilmsContext.Provider>
   );
 }
 
--- src/context/FilmsContext.tsx
+++ src/context/FilmsContext.tsx
@@ -1,1 +1,6 @@
 // implemente a estrutura do context aqui.
+import { createContext } from 'react';
+
+const FilmsContext = createContext({});
+
+export default FilmsContext;
